fffe970cac426637d5018a7af7a2e58e1f12d2dd
Aksh <aksht455@gmail.com>
2026-01-02 21:23:06 +0530

    new
    Co-Authored-By: Yash <156162167+yash08238@users.noreply.github.com>

--- backend/config/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  secretKey: process.env.JWT_SECRET || "your-default-secret-key",
-  port: process.env.PORT || 3000,
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-};
